f488f56f08f4316a9b3edda11bc0eb26243c66ff
Anna Galkina <anna.blackbird1@gmail.com>
2023-02-10 23:15:27 +0700

    Fix lastUserName

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -65,7 +65,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = users.length // need to fix
+    const totalUsers = users.length
     const lastUserName = users.length !==0 ? users[users.length-1].name : undefined// need to fix
 
     return (
